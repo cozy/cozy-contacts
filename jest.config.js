c6943b79bd9f6b37b9d2c81a50f4cbff0969584d
Yannick Lohse <contact@yannick-lohse.fr>
2018-02-05 15:21:08 +0100

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  setupFiles: ["<rootDir>/test/setup.js"]
+  moduleFileExtensions: ["js", "jsx", "json", "styl"],
+  setupFiles: ["<rootDir>/test/setup.js"],
+  moduleNameMapper: {
+    "\\.(png|gif|jpe?g|svg)$": "<rootDir>/test/__mocks__/fileMock.js",
+    styles: "identity-obj-proxy"
+  },
+  transformIgnorePatterns: ["node_modules/(?!cozy-ui)"]
 };
