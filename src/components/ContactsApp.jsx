17cc49acc12e3f2186d4a620856cff26340ee072
Enguerran <enguerran@ticabri.com>
2018-02-13 12:55:39 +0100

    style: update cozy-ui
    - buttons are resized
    - menu is extended

--- src/components/ContactsApp.jsx
+++ src/components/ContactsApp.jsx
@@ -66,7 +66,11 @@ class ContactsApp extends React.Component {
           />
         </div>
         {displayedContact && (
-          <Modal into="body" dismissAction={this.hideContactCard}>
+          <Modal
+            into="body"
+            dismissAction={this.hideContactCard}
+            size="xxlarge"
+          >
             <ModalContent>
               <TranslatedContactCard
                 contact={displayedContact}
