7b7ec0e4da310cb0aa0fbded00555e911fb4fa15
Yannick Lohse <contact@yannick-lohse.fr>
2018-03-15 17:05:03 +0100

    feat: using react-select

--- src/components/ContactGroups/ContactGroupManager.jsx
+++ src/components/ContactGroups/ContactGroupManager.jsx
@@ -1,35 +1,58 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button } from "cozy-ui/react";
-import SelectableGroupList from "./SelectableGroupList";
+import Select from "react-select";
+import Icon from "cozy-ui/react/Icon";
+import palette from "cozy-ui/stylus/settings/palette.json";
+
+import IconGroups from "../../assets/icons/groups.svg";
 
 const GroupCreationForm = () => null;
 
-class ContactGroupManager extends React.Component {
-  state = {
-    deployed: true
-  };
+const MainButton = ({ innerProps, children }) => (
+  <div {...innerProps}>
+    <Button theme="secondary">
+      <Icon icon={IconGroups} color={palette["coolGrey"]} />
+      Gérer les groupes
+    </Button>
+    <div style={{ width: 0, height: 0, overflow: "hidden" }}>{children}</div>
+  </div>
+);
 
-  toggleContactGroup = group => {
-    console.log("toggle group", group);
+const CustomOption = ({ label, isSelected, innerProps }) => (
+  <label {...innerProps} className="group-option">
+    <input
+      type="checkbox"
+      checked={isSelected}
+      className="group-option__checkbox"
+    />
+    {label}
+  </label>
+);
+
+class ContactGroupManager extends React.Component {
+  onChange = selectedGroups => {
+    console.log("changed groups", selectedGroups);
   };
 
   render() {
     const { contactGroups, allGroups } = this.props;
 
     return (
-      <div>
-        <Button theme="secondary" label="Gérer les groupes" />
-        <div>
-          <SelectableGroupList
-            groups={allGroups}
-            selected={contactGroups}
-            onToggleGroup={this.toggleContactGroup}
-          />
-          <hr />
-          <GroupCreationForm />
-        </div>
-      </div>
+      <Select
+        isMulti
+        defaultValue={contactGroups}
+        options={allGroups}
+        onChange={this.onChange}
+        hideSelectedOptions={false}
+        isSearchable={false}
+        getOptionLabel={group => group.name}
+        getOptionValue={group => group._id}
+        components={{
+          Option: CustomOption,
+          Control: MainButton
+        }}
+      />
     );
   }
 }
