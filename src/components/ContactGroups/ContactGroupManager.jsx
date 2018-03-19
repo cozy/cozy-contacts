d0956568fd5c6a7215c58c64fe36da828ca4a4b4
Yannick Lohse <contact@yannick-lohse.fr>
2018-03-19 16:20:58 +0100

    fix: use cozy-ui version of the Select

--- src/components/ContactGroups/ContactGroupManager.jsx
+++ src/components/ContactGroups/ContactGroupManager.jsx
@@ -1,39 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button } from "cozy-ui/react";
-import Select from "react-select";
+import Select, {
+  withCustomControl,
+  CheckboxOption
+} from "cozy-ui/react/select";
 import Icon from "cozy-ui/react/Icon";
 import palette from "cozy-ui/stylus/settings/palette.json";
 
 import IconGroups from "../../assets/icons/groups.svg";
 import IconDown from "../../assets/icons/down.svg";
 
-const MainButton = ({ innerProps, children }) => (
-  <div {...innerProps}>
-    <Button theme="secondary" size="small">
-      <Icon icon={IconGroups} color={palette["coolGrey"]} />
-      Gérer les groupes
-      <Icon
-        icon={IconDown}
-        color={palette["coolGrey"]}
-        width="12"
-        className="group-manager__indicator"
-      />
-    </Button>
-    <div className="group-manager__input">{children}</div>
-  </div>
+const MainButton = (
+  <Button theme="secondary" size="small">
+    <Icon icon={IconGroups} color={palette["coolGrey"]} />
+    Gérer les groupes
+    <Icon
+      icon={IconDown}
+      color={palette["coolGrey"]}
+      width="12"
+      className="group-manager__indicator"
+    />
+  </Button>
 );
 
-const CustomOption = ({ label, isSelected, innerProps }) => (
-  <div {...innerProps} className="group-option">
-    <input
-      type="checkbox"
-      checked={isSelected}
-      className="group-option__checkbox"
-    />
-    {label}
-  </div>
-);
+const MainButtonControl = withCustomControl(MainButton);
 
 const customStyles = {
   container: base => ({
@@ -58,8 +49,8 @@ const ContactGroupManager = ({
     getOptionLabel={group => group.name}
     getOptionValue={group => group._id}
     components={{
-      Option: CustomOption,
-      Control: MainButton
+      Option: CheckboxOption,
+      Control: MainButtonControl
     }}
     styles={customStyles}
   />
