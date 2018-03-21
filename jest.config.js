1339720b9d89ba763fd04f24718aafe259f0f587
Yannick Lohse <contact@yannick-lohse.fr>
2018-03-21 12:39:30 +0100

    fix: less greedy jest moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   moduleFileExtensions: ["js", "jsx", "json", "styl"],
+  transformIgnorePatterns: ["node_modules/(?!cozy-ui)"],
   setupFiles: ["<rootDir>/jestHelpers/setup.js"],
   moduleNameMapper: {
-    "\\.(png|gif|jpe?g)$": "<rootDir>/jestHelpers/mocks/fileMock.js",
-    "\\.svg$": "<rootDir>/jestHelpers/mocks/iconMock.js",
-    styles: "identity-obj-proxy"
-  },
-  transformIgnorePatterns: ["node_modules/(?!cozy-ui)"]
+    ".(png|gif|jpe?g)$": "<rootDir>/jestHelpers/mocks/fileMock.js",
+    ".svg$": "<rootDir>/jestHelpers/mocks/iconMock.js",
+    ".styl$": "identity-obj-proxy"
+  }
 };
