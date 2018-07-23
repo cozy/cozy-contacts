1fdac4e877806a7e58af1c3a540389f44402e5f3
Enguerran <engcolson@gmail.com>
2018-07-23 10:29:23 +0200

    refactor: Remove dead code

--- src/components/ContactsList/ContactsHeader.jsx
+++ src/components/ContactsList/ContactsHeader.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ContactsFilter = () => (
-  <div>
-    <select name="text">
-      <option value="value1">Value 1</option>
-      <option value="value2" selected>
-        Value 2
-      </option>
-      <option value="value3">Value 3</option>
-    </select>
-  </div>
-)
-
 const ContactsHeader = ({ renderActions }) => (
   <div className="topbar">
-    <div className="topbar__left">{false && <ContactsFilter />}</div>
+    <div className="topbar__left" />
     <div className="topbar__right">{renderActions()}</div>
   </div>
 )
