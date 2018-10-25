5344e0b295388c3e3e37cf9922a77174eaf33e84
Quentin Valmori <quentin.valmori@gmail.com>
2018-10-25 11:33:24 +0200

    chore: Remove fakeIntent

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
-import ContactsIntentButton from './Buttons/ContactsIntentButton'
 import { Button } from 'cozy-ui/react'
 import IconTeam from '../assets/icons/team.svg'
 import { translate } from 'cozy-ui/react/I18n'
 const Toolbar = ({ displayContactForm, displayVcardImport, t }) => {
-  const searchParams = new URL(window.location).searchParams
-  const fakeintentEnabled = searchParams.get('fakeintent') !== null
   return (
     <div className="actions">
-      {fakeintentEnabled && (
-        <ContactsIntentButton
-          label={'Select a Contact (intent)'}
-          action="PICK"
-        />
-      )}
-      {fakeintentEnabled && (
-        <ContactsIntentButton
-          label={'Create a Contact (intent)'}
-          action="CREATE"
-          data={{ me: true }}
-        />
-      )}
       <Button
         onClick={displayContactForm}
         icon="plus"
