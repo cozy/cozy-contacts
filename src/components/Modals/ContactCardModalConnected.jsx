df5982d379cf0fe199b96c48aef9ca6dd00fcabf
Quentin Valmori <quentin.valmori@gmail.com>
2018-10-26 12:04:20 +0200

    chore: fix travis

--- src/components/Modals/ContactCardModalConnected.jsx
+++ src/components/Modals/ContactCardModalConnected.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'React'
+import React, { Component } from 'react'
 import ContactCardModal from './ContactCardModal'
 import withModal from '../HOCs/withModal'
 class ContactCardModalConnected extends Component {
