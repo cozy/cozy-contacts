0e89625538c60e5305f6f01804d263482297ab84
Quentin Valmori <quentin.valmori@gmail.com>
2018-11-08 08:41:24 +0100

    chore: Add a test for ContactIdentity

---
+++ src/components/ContactCard/ContactIdentity.spec.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import ContactIdentity from './ContactIdentity'
+import AppLike from '../../tests/Applike'
+describe('ContactIdentity', () => {
+  it('should have the right informations', () => {
+    const contactWithoutName = {
+      email: [{ address: 'stephen.flinn@cirpria.me', primary: true }],
+      name: { familyName: 'Flinn', givenName: 'Stephen' },
+      groups: {
+        data: [
+          { _id: 'a', _type: 'io.cozy.contacts.groups' },
+          { _id: 'b', _type: 'io.cozy.contacts.groups' }
+        ]
+      }
+    }
+    const groupsMock = [
+      { _id: 'a', name: 'The A Team' },
+      { _id: 'b', name: 'The B Team' },
+      { _id: 'c', name: 'The C Team' }
+    ]
+
+    const app = mount(
+      <AppLike>
+        <ContactIdentity contact={contactWithoutName} groups={groupsMock} />
+      </AppLike>
+    )
+    //console.log('test', app.debug())
+    const avatar = app.find('Avatar')
+    expect(avatar.prop('text')).toEqual('SF')
+  })
+
+  it('should have the right informations even if the contact does not have a name', () => {
+    const contactWithoutName = {
+      email: [{ address: 'stephen.flinn@cirpria.me', primary: true }],
+      groups: {
+        data: [
+          { _id: 'a', _type: 'io.cozy.contacts.groups' },
+          { _id: 'b', _type: 'io.cozy.contacts.groups' }
+        ]
+      }
+    }
+    const groupsMock = [
+      { _id: 'a', name: 'The A Team' },
+      { _id: 'b', name: 'The B Team' },
+      { _id: 'c', name: 'The C Team' }
+    ]
+
+    const app = mount(
+      <AppLike>
+        <ContactIdentity contact={contactWithoutName} groups={groupsMock} />
+      </AppLike>
+    )
+    //console.log('test', app.debug())
+    const avatar = app.find('Avatar')
+    expect(avatar.prop('text')).toEqual('')
+  })
+})
