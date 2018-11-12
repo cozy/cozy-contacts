9047b198e94eb86b2223d507b4871230bc0d0dac
Quentin Valmori <quentin.valmori@gmail.com>
2018-11-12 16:13:02 +0100

    feat: Add flags feature

---
+++ src/helpers/flags.js
@@ -0,0 +1,20 @@
+import flag from 'cozy-flags'
+export const initFlags = () => {
+  let activateFlags = flag('switcher') === true ? true : false
+  if (process.env.NODE_ENV !== 'production' && flag('switcher') === null) {
+    activateFlags = true
+  }
+  const searchParams = new URL(window.location).searchParams
+  if (!activateFlags && searchParams.get('flags') !== null) {
+    activateFlags = true
+  }
+  if (activateFlags) {
+    flagsList()
+  }
+}
+
+const flagsList = () => {
+  flag('switcher', true)
+  flag('groupscreation')
+  flag('logs')
+}
