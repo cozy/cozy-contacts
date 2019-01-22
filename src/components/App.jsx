0303bf2e3fbec73b87a4db3911f7e427a9522c95
Quentin Valmori <quentin.valmori@gmail.com>
2019-01-22 19:31:45 +0100

    feat: Display Contacts in the CozyBar on smalscreen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
+/* global cozy */
 import React from 'react'
 import { PropTypes } from 'prop-types'
 import flow from 'lodash/flow'
-import { Alerter } from 'cozy-ui/transpiled/react'
 import { Main, Layout, Content } from 'cozy-ui/transpiled/react/Layout'
+import { Alerter, Title } from 'cozy-ui/transpiled/react'
+import withBreakpoints from 'cozy-ui/react/helpers/withBreakpoints'
+
 import { translate } from 'cozy-ui/transpiled/react/I18n'
 import { Query } from 'cozy-client'
 import flag, { FlagSwitcher } from 'cozy-flags'
@@ -19,6 +22,7 @@ import { DOCTYPE_CONTACTS } from '../helpers/doctypes'
 import ContactsListDataLoader from './ContactsList/ContactsListDataLoader'
 import Header from './Header'
 import Toolbar from './Toolbar'
+const { BarCenter } = cozy.bar
 
 const query = client => client.all(DOCTYPE_CONTACTS)
 
@@ -29,10 +33,22 @@ class ContactsApp extends React.Component {
   }
 
   render() {
-    const { t, deleteContact } = this.props
+    const {
+      t,
+      breakpoints: { isMobile },
+      deleteContact
+    } = this.props
 
     return (
       <Layout monocolumn="true">
+        {isMobile && (
+          <BarCenter>
+            {' '}
+            <Title>
+              <span className={'fil-path-title'}>Contacts</span>
+            </Title>
+          </BarCenter>
+        )}
         <Main>
           {flag('switcher') && <FlagSwitcher />}
           <ContactsSelectionBar trashAction={deleteContact} />
@@ -69,5 +85,6 @@ ContactsApp.propTypes = {
 export default flow(
   translate(),
   withContactsMutations,
-  container
+  container,
+  withBreakpoints()
 )(ContactsApp)
