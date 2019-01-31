994299e878d26c735a0eebeb80dea6abfdd8e20a
Cédric Messiant <cedric.messiant@gmail.com>
2019-01-31 11:22:33 +0100

    test: Use enzyme-to-json serializer for shallow rendering

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'styl'],
   transformIgnorePatterns: ['node_modules/(?!cozy-ui)'],
   setupFiles: ['<rootDir>/jestHelpers/setup.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
   moduleNameMapper: {
     '.(png|gif|jpe?g)$': '<rootDir>/jestHelpers/mocks/fileMock.js',
     '.svg$': '<rootDir>/jestHelpers/mocks/iconMock.js',
