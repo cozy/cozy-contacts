0101128bf534c076686595dcd7c3a4a716ed05b0
Cédric Messiant <cedric.messiant@gmail.com>
2019-03-11 11:38:28 +0100

    feat: Update cozy-client to 6.9.0 (new cozy metadata creation/update)

--- src/helpers/doctypes.js
+++ src/helpers/doctypes.js
@@ -1,3 +1,4 @@
 export const DOCTYPE_CONTACTS = 'io.cozy.contacts'
+export const DOCTYPE_CONTACTS_VERSION = 2
 export const DOCTYPE_CONTACT_GROUPS = 'io.cozy.contacts.groups'
 export const DOCTYPE_CONTACT_ACCOUNTS = 'io.cozy.contacts.accounts'
