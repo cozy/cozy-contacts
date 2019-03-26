bfe40cd197ca9d04e93c1064b0fbaf2e08aa15e7
Quentin Valmori <quentin.valmori@gmail.com>
2019-03-26 12:05:03 +0100

    chore: Fix ContactCard test

--- src/components/ContactCard/ContactCard.spec.jsx
+++ src/components/ContactCard/ContactCard.spec.jsx
@@ -8,7 +8,7 @@ import {
 } from '../../helpers/doctypes'
 
 describe('ContactCard', () => {
-  it('should match snapshot', () => {
+  it('should match snapshot when groups are available but no one is linked to the specified contact', () => {
     const contact = {
       _id: 'c6899688-6cc6-4ffb-82d4-ab9f9b82c582',
       _rev: '1-9368a4f2e467c449f4a1f5171a784aa8',
@@ -51,14 +51,14 @@ describe('ContactCard', () => {
     const tree = renderer
       .create(
         <AppLike>
-          <ContactCard contact={contact} groups={groups} />
+          <ContactCard contact={contact} allGroups={groups} />
         </AppLike>
       )
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
 
-  it('should match snapshot with accounts', () => {
+  it('should match snapshot with accounts without groups', () => {
     const contact = {
       _id: 'c6899688-6cc6-4ffb-82d4-ab9f9b82c582',
       _rev: '1-9368a4f2e467c449f4a1f5171a784aa8',
@@ -95,7 +95,60 @@ describe('ContactCard', () => {
     const tree = renderer
       .create(
         <AppLike>
-          <ContactCard contact={contact} groups={groups} />
+          <ContactCard contact={contact} allGroups={groups} />
+        </AppLike>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should match snapshot with accounts and groups', () => {
+    const contact = {
+      _id: 'c6899688-6cc6-4ffb-82d4-ab9f9b82c582',
+      _rev: '1-9368a4f2e467c449f4a1f5171a784aa8',
+      address: [],
+      birthday: '1998-06-03',
+      email: [{ address: 'rikki.white@celmax.name', primary: true }],
+      name: { familyName: 'White', givenName: 'Rikki' },
+      phone: [],
+      accounts: {
+        doctype: DOCTYPE_CONTACT_ACCOUNTS,
+        data: [
+          {
+            id: '8336bd7c-ea8a-4a82-8335-799a599e7ebc',
+            name: 'john.doe@gmail.com',
+            sourceAccount: '55b4b61a-6838-4219-91fa-2b846192f8aa',
+            type: 'konnector-google'
+          },
+          {
+            id: '53602dea-6e21-4613-97d6-dde4f5f4dac0',
+            name: 'john.doe+inactive@gmail.com',
+            sourceAccount: null,
+            type: 'konnector-google'
+          }
+        ]
+      },
+      groups: {
+        doctype: DOCTYPE_CONTACT_GROUPS,
+        data: [
+          {
+            _id: 'test'
+          }
+        ]
+      },
+      unsupportedField: 'unsupportedField'
+    }
+
+    const groups = [
+      {
+        _id: 'test',
+        name: 'toto'
+      }
+    ]
+    const tree = renderer
+      .create(
+        <AppLike>
+          <ContactCard contact={contact} allGroups={groups} />
         </AppLike>
       )
       .toJSON()
