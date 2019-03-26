3f104dcd339efad9091484246b5d4cb1597dcfa2
Quentin Valmori <quentin.valmori@gmail.com>
2019-03-26 12:06:17 +0100

    fix: No need for the key attribute in this component

--- src/components/Components/GroupItem.jsx
+++ src/components/Components/GroupItem.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 
-const GroupItem = ({ children, key }) => (
-  <li
-    key={key}
-    className="u-dib u-slateGrey u-fz-small u-p-half u-mr-half u-w-auto u-maw-4 u-bg-paleGrey u-ellipsis"
-  >
+const GroupItem = ({ children }) => (
+  <li className="u-dib u-slateGrey u-fz-small u-p-half u-mr-half u-w-auto u-maw-4 u-bg-paleGrey u-ellipsis">
     {children}
   </li>
 )
