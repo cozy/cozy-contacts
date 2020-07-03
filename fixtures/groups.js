22a1ea7b9e5f4d16d2ec15b9bdffc0c0e20a32ba
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-07-03 16:10:59 +0200

    test: Add fixtures for contacts and groups

---
+++ fixtures/groups.js
@@ -0,0 +1,56 @@
+export const fakedAllGroups = [
+  {
+    id: '5b49553c5916cf7b5b2a5f48600078a8',
+    _id: '5b49553c5916cf7b5b2a5f48600078a8',
+    _type: 'io.cozy.contacts.groups',
+    _rev: '1-7862def64fb044932d3264e2f8477454',
+    cozyMetadata: {
+      createdAt: '2019-07-15T11:22:13.551Z',
+      createdByApp: 'Contacts',
+      createdByAppVersion: '0.8.5',
+      metadataVersion: 1,
+      updatedAt: '2019-07-15T11:22:13.551Z',
+      updatedByApps: [
+        { date: '2019-07-15T11:22:13.551Z', slug: 'Contacts', version: '0.8.5' }
+      ]
+    },
+    metadata: { version: 1 },
+    name: '2018-2019 Enseignants'
+  },
+  {
+    id: '8cb7ea7fe264260e997529439b0091c0',
+    _id: '8cb7ea7fe264260e997529439b0091c0',
+    _type: 'io.cozy.contacts.groups',
+    _rev: '1-8b92242f1ccaca20e7862306cddbe37f',
+    cozyMetadata: {
+      createdAt: '2020-04-15T06:46:20.128Z',
+      createdByApp: 'Contacts',
+      createdByAppVersion: '0.8.7',
+      metadataVersion: 1,
+      updatedAt: '2020-04-15T06:46:20.128Z',
+      updatedByApps: [
+        { date: '2020-04-15T06:46:20.128Z', slug: 'Contacts', version: '0.8.7' }
+      ]
+    },
+    metadata: { version: 1 },
+    name: 'ez'
+  },
+  {
+    id: '18c031b0ac9f47cdc48b275b1900726d',
+    _id: '18c031b0ac9f47cdc48b275b1900726d',
+    _type: 'io.cozy.contacts.groups',
+    _rev: '1-f438477a749fa0b90cde2e7c53a41bd6',
+    cozyMetadata: {
+      createdAt: '2019-11-08T07:45:55.424Z',
+      createdByApp: 'Contacts',
+      createdByAppVersion: '0.8.6',
+      metadataVersion: 1,
+      updatedAt: '2019-11-08T07:45:55.424Z',
+      updatedByApps: [
+        { date: '2019-11-08T07:45:55.424Z', slug: 'Contacts', version: '0.8.6' }
+      ]
+    },
+    metadata: { version: 1 },
+    name: 'groupe test'
+  }
+]
