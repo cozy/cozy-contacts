79694c7fb3a6aaebd97c8ebf9abd9ece66f206cc
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-07-08 14:00:22 +0200

    test: Refacto tests and update snapshots

--- src/components/ContactCard/ContactForm/index.spec.jsx
+++ src/components/ContactCard/ContactForm/index.spec.jsx
@@ -1,39 +1,69 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
 import { mount } from 'enzyme'
 import ContactForm from './index'
 import { I18n } from 'cozy-ui/transpiled/react/I18n'
+
+import { render, fireEvent } from '@testing-library/react'
+import { createMockClient } from 'cozy-client'
+import AppLike from '../../../tests/Applike'
 
 import langEn from '../../../locales/en.json'
 import { johnDoeContact as contact } from '../../../helpers/testData'
 
 const dictRequire = () => langEn
 
+const client = createMockClient({})
+const setup = ({ contact }) => {
+  return {
+    contact,
+    onSubmit: () => jest.fn(),
+    onCancel: () => jest.fn()
+  }
+}
+
+const labels = [
+  'Firstname',
+  'Lastname',
+  'Phone',
+  'Email',
+  'Address',
+  'Cozy URL',
+  'Company',
+  'Birthday',
+  'Notes'
+]
+
 describe('ContactForm', () => {
-  it('should match snapshot', () => {
-    const tree = renderer
-      .create(
-        <I18n lang="en" dictRequire={dictRequire}>
-          <ContactForm onSubmit={() => {}} onCancel={() => {}} />
-        </I18n>
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+  it(`should contains ${labels.length} fields and labels`, () => {
+    const props = setup({ contact: {} })
+    const jsx = (
+      <AppLike client={client}>
+        <ContactForm {...props} />
+      </AppLike>
+    )
+    const { getByLabelText, getAllByRole } = render(jsx)
+    expect(getAllByRole('label')).toHaveLength(labels.length)
+    labels.map(label => {
+      expect(getByLabelText(label)).toBeTruthy()
+    })
   })
 
   it('should fill form with contact data', () => {
-    const tree = renderer
-      .create(
-        <I18n lang="en" dictRequire={dictRequire}>
-          <ContactForm
-            contact={contact}
-            onSubmit={() => {}}
-            onCancel={() => {}}
-          />
-        </I18n>
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+    const props = setup({ contact })
+    const jsx = (
+      <AppLike client={client}>
+        <ContactForm {...props} />
+      </AppLike>
+    )
+    const { getByDisplayValue } = render(jsx)
+    expect(getByDisplayValue(contact.name.givenName)).toBeTruthy()
+    expect(getByDisplayValue(contact.name.familyName)).toBeTruthy()
+    expect(getByDisplayValue(contact.phone[0].number)).toBeTruthy()
+    expect(getByDisplayValue(contact.email[0].address)).toBeTruthy()
+    expect(getByDisplayValue(contact.address[0].formattedAddress)).toBeTruthy()
+    expect(getByDisplayValue(contact.cozy[0].url)).toBeTruthy()
+    expect(getByDisplayValue(contact.company)).toBeTruthy()
+    expect(getByDisplayValue(contact.note)).toBeTruthy()
   })
 
   it('should submit a well formatted contact', () => {
@@ -66,6 +96,37 @@ describe('ContactForm', () => {
     })
 
     form.find('form').simulate('submit')
+  })
+
+  it('should change inputs value', async () => {
+    const testFields = {
+      Firstname: 'Jean-Claude',
+      Lastname: 'Van Cozy',
+      Phone: '+33678987654',
+      Email: 'jcvc@cozy.cloud',
+      Address: '18 rue des fleurs, Pecado',
+      'Cozy URL': 'https://jcvd.cozy.cloud',
+      Company: 'Cozy CLoud',
+      Birthday: '31/12/2015',
+      Notes: 'Whatever.'
+    }
+
+    const props = setup({ contact: {} })
+    const jsx = (
+      <AppLike client={client}>
+        <ContactForm {...props} />
+      </AppLike>
+    )
+    const { findByDisplayValue, getByLabelText } = render(jsx)
+
+    labels.map(label => {
+      fireEvent.change(getByLabelText(label), {
+        target: { value: testFields[label] }
+      })
+    })
+
+    expect(await findByDisplayValue(testFields['Firstname'])).toBeTruthy()
+    expect(await findByDisplayValue(testFields['Notes'])).toBeTruthy()
   })
 
   it('should submit empty fields', () => {
