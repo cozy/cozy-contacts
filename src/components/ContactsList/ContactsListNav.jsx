2ff33d3e2b88b935241799b3e13dbb5f3b83061e
Pauline Thomas <pauline@bridge.audio>
2020-09-25 16:42:14 +0200

    Feat(contact.list.navigation): add propTypes

--- src/components/ContactsList/ContactsListNav.jsx
+++ src/components/ContactsList/ContactsListNav.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PropTypes } from 'prop-types'
 import { unicodeNumberToChar } from '../../helpers/utils'
 
 const ContactsListNav = ({ handleClick, activeItems }) => {
@@ -15,5 +16,10 @@ const ContactsListNav = ({ handleClick, activeItems }) => {
     </div >
   )
 };
+
+ContactsListNav.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  activeItems: PropTypes.array.isRequired
+}
 
 export default ContactsListNav;
