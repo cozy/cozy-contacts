2e6b2702fa76f1e1b2788a9cb647596930fad22d
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-10-06 17:07:34 +0200

    refactor: Extract filtering function
    test refactoring + add some tests for filtering

--- src/components/ContentResult.spec.jsx
+++ src/components/ContentResult.spec.jsx
@@ -1,177 +1,183 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-
-import { createMockClient } from 'cozy-client'
+import { render, fireEvent } from '@testing-library/react'
 
 import AppLike from '../tests/Applike'
 import ContentResult from './ContentResult'
 import { groups } from '../helpers/testData'
 
-const client = createMockClient({})
+const mockedContacts = {
+  withIndexes: [
+    {
+      name: { givenName: 'Jane', familyName: 'Doe' },
+      fullname: 'Jane Doe',
+      displayName: 'Jane Doe',
+      indexes: { byFamilyNameGivenNameEmailCozyUrl: 'Doejane' }
+    }
+  ],
+  withoutIndexes: [
+    {
+      name: { givenName: 'William', familyName: 'Wallace' }
+    }
+  ],
+  withGroup: [
+    {
+      displayName: 'Jack Ingroup',
+      indexes: { byFamilyNameGivenNameEmailCozyUrl: 'jackingroup' },
+      relationships: {
+        groups: {
+          data: groups
+        }
+      }
+    }
+  ]
+}
 
-const contactsWithIndexes = [
-  {
-    name: { givenName: 'Jane', familyName: 'Doe' },
-    fullname: 'Jane Doe',
-    displayName: 'Jane Doe',
-    indexes: { byFamilyNameGivenNameEmailCozyUrl: 'Doejane' }
+const setup = ({
+  hasServiceBeenLaunched = true,
+  contactsWithIndexesResult = {
+    data: mockedContacts.withIndexes,
+    fetchStatus: 'loaded',
+    hasMore: false,
+    fetchMore: jest.fn()
+  },
+  contactsWithNoIndexesResult = {
+    data: mockedContacts.withoutIndexes,
+    fetchStatus: 'loaded',
+    hasMore: false,
+    fetchMore: jest.fn()
+  },
+  allGroupsResult = {
+    data: groups,
+    fetchStatus: 'loaded'
   }
-]
-const contactsWithNoIndexes = [
-  {
-    name: { givenName: 'William', familyName: 'Wallace' }
-  }
-]
-const allGroupsResult = {
-  data: groups,
-  fetchStatus: 'loaded'
+} = {}) => {
+  const root = render(
+    <AppLike>
+      <ContentResult
+        hasServiceBeenLaunched={hasServiceBeenLaunched}
+        contactsWithIndexesResult={contactsWithIndexesResult}
+        contactsWithNoIndexesResult={contactsWithNoIndexesResult}
+        allGroupsResult={allGroupsResult}
+      />
+    </AppLike>
+  )
+  return { root }
 }
 
 describe('ContentResult', () => {
   it('should show a spinner if data has not been loaded', () => {
-    const hasServiceBeenLaunched = false
-    const contactsWithIndexesResult = {
-      data: [{}],
-      fetchStatus: 'pending',
-      hasMore: false,
-      fetchMore: jest.fn()
-    }
-    const contactsWithNoIndexesResult = {
-      data: [{}],
-      fetchStatus: 'pending',
-      hasMore: false,
-      fetchMore: jest.fn()
-    }
-    const props = {
-      hasServiceBeenLaunched,
-      contactsWithIndexesResult,
-      contactsWithNoIndexesResult,
-      allGroupsResult
-    }
-    const jsx = (
-      <AppLike client={client}>
-        <ContentResult {...props} />
-      </AppLike>
-    )
-    const { getByTestId } = render(jsx)
+    const { root } = setup({
+      hasServiceBeenLaunched: false,
+      contactsWithIndexesResult: {
+        data: [{}],
+        fetchStatus: 'pending',
+        hasMore: false,
+        fetchMore: jest.fn()
+      },
+      contactsWithNoIndexesResult: {
+        data: [{}],
+        fetchStatus: 'pending',
+        hasMore: false,
+        fetchMore: jest.fn()
+      }
+    })
+    const { getByTestId } = root
     expect(getByTestId('contactSpinner'))
   })
-
   it('should show a spinner if one query is still loading', () => {
-    const hasServiceBeenLaunched = false
-    const contactsWithIndexesResult = {
-      data: contactsWithIndexes,
-      fetchStatus: 'loaded',
-      hasMore: false,
-      fetchMore: jest.fn()
-    }
-    const contactsWithNoIndexesResult = {
-      data: [{}],
-      fetchStatus: 'loading',
-      hasMore: false,
-      fetchMore: jest.fn()
-    }
-    const props = {
-      hasServiceBeenLaunched,
-      contactsWithIndexesResult,
-      contactsWithNoIndexesResult,
-      allGroupsResult
-    }
-    const jsx = (
-      <AppLike client={client}>
-        <ContentResult {...props} />
-      </AppLike>
-    )
-    const { getByTestId } = render(jsx)
+    const { root } = setup({
+      hasServiceBeenLaunched: false,
+      contactsWithIndexesResult: {
+        data: mockedContacts.withIndexes,
+        fetchStatus: 'loaded',
+        hasMore: false,
+        fetchMore: jest.fn()
+      },
+      contactsWithNoIndexesResult: {
+        data: [{}],
+        fetchStatus: 'loading',
+        hasMore: false,
+        fetchMore: jest.fn()
+      }
+    })
+    const { getByTestId } = root
     expect(getByTestId('contactSpinner'))
   })
-
   it('should show a spinner if queries are both loaded but still with more data to fetch', () => {
-    const hasServiceBeenLaunched = false
-    const contactsWithIndexesResult = {
-      data: contactsWithIndexes,
-      fetchStatus: 'loaded',
-      hasMore: true,
-      fetchMore: jest.fn()
-    }
-    const contactsWithNoIndexesResult = {
-      data: contactsWithNoIndexes,
-      fetchStatus: 'loaded',
-      hasMore: true,
-      fetchMore: jest.fn()
-    }
-    const props = {
-      hasServiceBeenLaunched,
-      contactsWithIndexesResult,
-      contactsWithNoIndexesResult,
-      allGroupsResult
-    }
-    const jsx = (
-      <AppLike client={client}>
-        <ContentResult {...props} />
-      </AppLike>
-    )
-    const { getByTestId } = render(jsx)
+    const { root } = setup({
+      hasServiceBeenLaunched: false,
+      contactsWithIndexesResult: {
+        data: mockedContacts.withIndexes,
+        fetchStatus: 'loaded',
+        hasMore: true,
+        fetchMore: jest.fn()
+      },
+      contactsWithNoIndexesResult: {
+        data: mockedContacts.withoutIndexes,
+        fetchStatus: 'loaded',
+        hasMore: true,
+        fetchMore: jest.fn()
+      }
+    })
+    const { getByTestId } = root
     expect(getByTestId('contactSpinner'))
   })
-
   it('should have empty section (for contacts without indexes) if service has been launched', () => {
-    const hasServiceBeenLaunched = true
-    const contactsWithIndexesResult = {
-      data: contactsWithIndexes,
-      fetchStatus: 'loaded',
-      hasMore: false,
-      fetchMore: jest.fn()
-    }
-    const contactsWithNoIndexesResult = {
-      data: contactsWithNoIndexes,
-      fetchStatus: 'loaded',
-      hasMore: false,
-      fetchMore: jest.fn()
-    }
-    const props = {
-      hasServiceBeenLaunched,
-      contactsWithIndexesResult,
-      contactsWithNoIndexesResult,
-      allGroupsResult
-    }
-
-    const jsx = (
-      <AppLike client={client}>
-        <ContentResult {...props} />
-      </AppLike>
-    )
-
-    const { getByText } = render(jsx)
+    const { root } = setup()
+    const { getByText } = root
     expect(getByText('EMPTY'))
   })
-
   it('should not have empty section (for contacts without indexes) if service has not been launched', () => {
-    const hasServiceBeenLaunched = false
-    const contactsWithIndexesResult = {
-      data: contactsWithIndexes,
-      fetchStatus: 'loaded',
-      hasMore: false,
-      fetchMore: jest.fn()
-    }
-    const contactsWithNoIndexesResult = {
-      data: contactsWithNoIndexes,
-      fetchStatus: 'loaded',
-      hasMore: false,
-      fetchMore: jest.fn()
-    }
-    const props = {
-      hasServiceBeenLaunched,
-      contactsWithIndexesResult,
-      contactsWithNoIndexesResult,
-      allGroupsResult
-    }
-    const jsx = (
-      <AppLike client={client}>
-        <ContentResult {...props} />
-      </AppLike>
-    )
-    const { queryByText } = render(jsx)
+    const { root } = setup({
+      hasServiceBeenLaunched: false
+    })
+    const { queryByText } = root
     expect(queryByText('EMPTY')).toBeNull()
+  })
+})
+
+describe('ContentResult - filtering', () => {
+  it('should show only filtered contacts after selecting a group filter', () => {
+    const { root } = setup({
+      contactsWithIndexesResult: {
+        data: [mockedContacts.withIndexes[0], mockedContacts.withGroup[0]]
+      }
+    })
+
+    const { getByText, queryByText } = root
+
+    // contacts with and without group should be present
+    expect(getByText('Ingroup'))
+    expect(getByText('Doe'))
+
+    fireEvent.click(getByText('All contacts'))
+    fireEvent.click(getByText(groups[0].name))
+
+    // should not return an empty contact list
+    expect(
+      queryByText(
+        'You will soon be able to import your contacts from other services (iCloud, Facebook...).'
+      )
+    ).toBeNull()
+
+    // contacts without group should not be present
+    expect(getByText('Ingroup'))
+    expect(queryByText('Doe')).toBeNull()
+  })
+
+  it('should show empty list after filtering contacts with no group', () => {
+    const { root } = setup()
+
+    const { getByText } = root
+
+    fireEvent.click(getByText('All contacts'))
+    fireEvent.click(getByText(groups[0].name))
+
+    // should return an empty contact list
+    expect(
+      getByText(
+        'You will soon be able to import your contacts from other services (iCloud, Facebook...).'
+      )
+    )
   })
 })
--- src/helpers/groups.js
+++ src/helpers/groups.js
@@ -1,4 +1,6 @@
 import differenceBy from 'lodash/differenceBy'
+import filter from 'lodash/filter'
+import get from 'lodash/get'
 
 export const isExistingGroup = (groupsAlreadyCreated, groupToCreate) => {
   const isNameAlreadyUsed =
@@ -18,4 +20,20 @@ export const updateContactGroups = (contact, nextGroups) => {
   else if (toRemove.length > 0)
     contact.groups.removeById(toRemove.map(({ _id }) => _id))
   // we can't do both at the same time right now, see https://github.com/cozy/cozy-client/issues/358
+}
+
+/**
+ * Filter contacts by group
+ * @param {array} contacts - Array of io.cozy.contact documents
+ * @param {object} selectedGroup - Group to filter on
+ * @returns Array of filtered contacts
+ */
+export const filterContactsByGroup = (contacts, selectedGroup) => {
+  const filterRule = contact =>
+    get(contact, 'relationships.groups.data', []).find(
+      group => group._id === selectedGroup._id
+    )
+
+  const filteredContacts = filter(contacts, filterRule)
+  return filteredContacts
 }
