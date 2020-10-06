aa5ab9ebcb99c6c6b44958555693db824f60b914
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-10-06 12:27:30 +0200

    test: Add breakpoints provider

--- src/tests/Applike.jsx
+++ src/tests/Applike.jsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 
 import { CozyProvider } from 'cozy-client'
 import I18n from 'cozy-ui/transpiled/react/I18n'
+import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
 import langEn from '../locales/en.json'
 import configureStore from '../store/configureStore'
@@ -12,13 +13,15 @@ import { ContextProvider } from '../components/Context'
 const store = configureStore(getCozyClient(), null, {})
 
 const AppLike = ({ children, client }) => (
-  <Provider store={store}>
-    <CozyProvider client={client || getCozyClient()}>
-      <I18n lang={'en'} dictRequire={() => langEn}>
-        <ContextProvider>{children}</ContextProvider>
-      </I18n>
-    </CozyProvider>
-  </Provider>
+  <BreakpointsProvider>
+    <Provider store={store}>
+      <CozyProvider client={client || getCozyClient()}>
+        <I18n lang={'en'} dictRequire={() => langEn}>
+          <ContextProvider>{children}</ContextProvider>
+        </I18n>
+      </CozyProvider>
+    </Provider>
+  </BreakpointsProvider>
 )
 
 AppLike.defaultProps = {
