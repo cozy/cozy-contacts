5c7ffe57c87d80a62a8079dbe83a1e4105e6203a
ugogo <ugo.j.onali@gmail.com>
2020-10-07 12:21:42 +0200

    test: add group scrolling

--- src/components/ContactsList/ContactsList.spec.js
+++ src/components/ContactsList/ContactsList.spec.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer'
 import { mount } from 'enzyme'
 
 import AppLike from '../../tests/Applike'
-import ContactsList from './ContactsList'
+import ContactsList, { getGroupId } from './ContactsList'
 
 import { johnDoeContact } from '../../helpers/testData'
 
@@ -13,6 +13,24 @@ const fakeContactWithIndexes = {
     byFamilyNameGivenNameEmailCozyUrl: 'john.doe@cozycloud.cc'
   }
 }
+
+const firstContactLetter = fakeContactWithIndexes.fullname[0].toLowerCase()
+
+const fireKeyupEvent = key => {
+  const keyupEvent = new KeyboardEvent('keyup', { key })
+  document.dispatchEvent(keyupEvent)
+}
+
+let scrollIntoViewSpy
+
+beforeAll(() => {
+  scrollIntoViewSpy = jest.fn()
+  window.HTMLElement.prototype.scrollIntoView = scrollIntoViewSpy
+})
+
+afterAll(() => {
+  scrollIntoViewSpy.mockRestore()
+})
 
 describe('ContactsList', () => {
   it('should match the snapshot', () => {
@@ -36,4 +54,36 @@ describe('ContactsList', () => {
     const contactsemptylist = contactslist.find('ContactsEmptyList')
     expect(contactsemptylist).toBeDefined()
   })
+
+  it('scrolls to the group on keyup', () => {
+    const contactsListInstance = (
+      <AppLike>
+        <ContactsList contacts={[fakeContactWithIndexes]} />
+      </AppLike>
+    )
+    const contactslist = mount(contactsListInstance)
+    const group = contactslist.find(`.${getGroupId(firstContactLetter)}`)
+    const groupInstance = group.instance()
+
+    // renders a group
+    expect(groupInstance).toBeDefined()
+
+    fireKeyupEvent(firstContactLetter)
+
+    // test (smooth) scrolling
+    expect(scrollIntoViewSpy).toHaveBeenCalledTimes(1)
+    expect(scrollIntoViewSpy).toHaveBeenCalledWith({ behavior: 'smooth' })
+  })
+
+  // it('does not scroll when a modal is open', () => {
+  //   const contactsListInstance = (
+  //     <AppLike>
+  //       <ContactsList contacts={[fakeContactWithIndexes]} show />
+  //     </AppLike>
+  //   )
+
+  //   mount(contactsListInstance)
+  //   fireKeyupEvent(firstContactLetter)
+  //   expect(scrollIntoViewSpy).toHaveBeenCalledTimes(0)
+  // })
 })
