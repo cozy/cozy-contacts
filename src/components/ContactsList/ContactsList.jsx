dd977a3188ef794de55ff6e68b01822da1f208c7
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-10-14 11:49:58 +0200

    refactor: ContactsList to func comp

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,48 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import flag from 'cozy-flags'
 import Button from 'cozy-ui/transpiled/react/Button'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import ContactsEmptyList from './ContactsEmptyList'
 import CategorizedList from './CategorizedList'
 import UncategorizedList from './UncategorizedList'
 import withSelection from '../Selection/selectionContainer'
 
-class ContactsList extends Component {
-  render() {
-    const { clearSelection, contacts, selection, selectAll, t } = this.props
+const ContactsList = ({ contacts, clearSelection, selection, selectAll }) => {
+  const { t } = useI18n()
 
-    if (contacts.length === 0) {
-      return <ContactsEmptyList />
-    }
+  if (contacts.length === 0) {
+    return <ContactsEmptyList />
+  }
 
   const isSearched = false // TODO use context to determine bool result according to search input
   const List = isSearched ? UncategorizedList : CategorizedList
   const isAllContactsSelected = contacts.length === selection.length
 
-    const handleAllContactSelection = () => {
-      isAllContactsSelected ? clearSelection() : selectAll(contacts)
-    }
-
-    return (
-      <div className="list-wrapper">
-        {flag('select-all-contacts') && (
-          <div>
-            <Button
-              label={
-                isAllContactsSelected ? t('unselect-all') : t('select-all')
-              }
-              theme="secondary"
-              onClick={handleAllContactSelection}
-            />
-          </div>
-        )}
-        <List contacts={contacts} />
-      </div>
-    )
+  const handleAllContactSelection = () => {
+    isAllContactsSelected ? clearSelection() : selectAll(contacts)
   }
+
+  return (
+    <div className="list-wrapper">
+      {flag('select-all-contacts') && (
+        <div>
+          <Button
+            label={isAllContactsSelected ? t('unselect-all') : t('select-all')}
+            theme="secondary"
+            onClick={handleAllContactSelection}
+          />
+        </div>
+      )}
+      <List contacts={contacts} />
+    </div>
+  )
 }
 
 ContactsList.propTypes = {
@@ -50,4 +46,4 @@ ContactsList.propTypes = {
 }
 ContactsList.defaultProps = {}
 
-export default translate()(withSelection(ContactsList))
+export default withSelection(ContactsList)
