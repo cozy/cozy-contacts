ccedecc663bdf9d6b97b44ce34c997f2a39fbcb6
Louis Chemineau <louis@chmn.me>
2021-01-30 12:10:14 +0100

    Add keypress navigation
    + Handle keypress event in CategorizedList to jump to a contact category
    + Show an Alert to inform the user about the navigation

--- src/components/ContactsList/CategorizedList.jsx
+++ src/components/ContactsList/CategorizedList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import Alerter from 'cozy-ui/transpiled/react/Alerter'
 
 import ContactHeaderRow from './ContactHeaderRow'
 import ContactsSubList from './ContactsSubList'
@@ -11,10 +12,23 @@ const CategorizedList = ({ contacts }) => {
   const { t } = useI18n()
   const categorizedContacts = categorizeContacts(contacts, t('empty-list'))
 
+  document.onkeypress = function handleKeyPress(event) {
+    const contactCategory = document.getElementsByClassName(
+      `contact-category-${event.key.toLowerCase()}`
+    )
+
+    if (contactCategory.length == 0) {
+      return
+    }
+
+    Alerter.info(`Jump To ${event.key.toUpperCase()}`)
+    contactCategory[0].scrollIntoView()
+  }
+
   return (
     <ol className="list-contact">
       {Object.entries(categorizedContacts).map(([header, contacts]) => (
-        <li key={`cat-${header}`}>
+        <li key={`cat-${header}`} className={'contact-category-' + header}>
           <ContactHeaderRow key={header} header={header} />
           <ContactsSubList contacts={contacts} />
         </li>
