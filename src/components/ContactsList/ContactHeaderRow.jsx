5ecf77e12347c82fea2d607eff93b57d349d9d8a
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-02-04 11:28:22 +0100

    refactor: Remove divider css class

--- src/components/ContactsList/ContactHeaderRow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-const ContactHeaderRow = props => <div className="divider">{props.header}</div>
-
-ContactHeaderRow.propTypes = {
-  header: PropTypes.string.isRequired
-}
-export default ContactHeaderRow
