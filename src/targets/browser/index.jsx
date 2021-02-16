62e6d731e505b8e918aa6550d87ed40d76e9f67b
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-02-16 11:17:21 +0100

    fix: Disable global styles
    With MUI V4, it is possible to generate deterministic class names.
    In the case of multiple react roots, it is necessary to disable this
    feature. Since we have the cozy-bar root, we need to disable the
    feature.
    
    https://material-ui.com/styles/api/#stylesprovider
    
    Otherwise generated class names clash with others and some text become
    the wrong color during the usage of the app (especially visible on
    mobile).

--- src/targets/browser/index.jsx
+++ src/targets/browser/index.jsx
@@ -3,6 +3,10 @@ import 'cozy-ui/transpiled/react/stylesheet.css'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
+import {
+  StylesProvider,
+  createGenerateClassName
+} from '@material-ui/core/styles'
 
 import { CozyProvider } from 'cozy-client'
 import { I18n } from 'cozy-ui/transpiled/react/I18n'
@@ -15,24 +19,37 @@ import setupApp from './setupApp'
 import { SelectedGroupProvider } from '../../components/Contexts/SelectedGroup'
 import { SearchProvider } from '../../components/Contexts/Search'
 
+/*
+With MUI V4, it is possible to generate deterministic class names.
+In the case of multiple react roots, it is necessary to disable this
+feature. Since we have the cozy-bar root, we need to disable the
+feature.
+https://material-ui.com/styles/api/#stylesprovider
+*/
+const generateClassName = createGenerateClassName({
+  disableGlobal: true
+})
+
 const init = () => {
   const { root, store, client, lang, polyglot } = setupApp()
 
   render(
     <Provider store={store}>
-      <CozyProvider client={client}>
-        <I18n lang={lang} polyglot={polyglot}>
-          <MuiCozyTheme>
-            <BreakpointsProvider>
-              <SelectedGroupProvider>
-                <SearchProvider>
-                  <App />
-                </SearchProvider>
-              </SelectedGroupProvider>
-            </BreakpointsProvider>
-          </MuiCozyTheme>
-        </I18n>
-      </CozyProvider>
+      <StylesProvider generateClassName={generateClassName}>
+        <CozyProvider client={client}>
+          <I18n lang={lang} polyglot={polyglot}>
+            <MuiCozyTheme>
+              <BreakpointsProvider>
+                <SelectedGroupProvider>
+                  <SearchProvider>
+                    <App />
+                  </SearchProvider>
+                </SelectedGroupProvider>
+              </BreakpointsProvider>
+            </MuiCozyTheme>
+          </I18n>
+        </CozyProvider>
+      </StylesProvider>
     </Provider>,
     root
   )
