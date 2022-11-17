72ad8234e5f0b3f831d76dd9daea7432554eed35
Théo Poizat <hello@zatteo.com>
2022-11-17 12:16:41 +0100

    refactor: Create an AppProviders component to increase lisibility

--- src/targets/browser/index.jsx
+++ src/targets/browser/index.jsx
@@ -1,42 +1,20 @@
 import 'cozy-ui/transpiled/react/stylesheet.css'
 
 import React from 'react'
-import { Provider } from 'react-redux'
 import { render } from 'react-dom'
 
-import { CozyProvider } from 'cozy-client'
-import { I18n } from 'cozy-ui/transpiled/react/I18n'
-import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme'
-import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
-import { WebviewIntentProvider } from 'cozy-intent'
-
+import AppProviders from 'components/AppProviders'
 import AppRouter from 'components/AppRouter'
-import '../../styles/index.styl'
 import setupApp from './setupApp'
-import { SelectedGroupProvider } from '../../components/Contexts/SelectedGroup'
-import { SearchProvider } from '../../components/Contexts/Search'
+import '../../styles/index.styl'
 
 const init = () => {
   const { root, store, client, lang, polyglot } = setupApp()
 
   render(
-    <WebviewIntentProvider>
-      <Provider store={store}>
-        <CozyProvider client={client}>
-          <I18n lang={lang} polyglot={polyglot}>
-            <MuiCozyTheme>
-              <BreakpointsProvider>
-                <SelectedGroupProvider>
-                  <SearchProvider>
-                    <AppRouter />
-                  </SearchProvider>
-                </SelectedGroupProvider>
-              </BreakpointsProvider>
-            </MuiCozyTheme>
-          </I18n>
-        </CozyProvider>
-      </Provider>
-    </WebviewIntentProvider>,
+    <AppProviders store={store} client={client} lang={lang} polyglot={polyglot}>
+      <AppRouter />
+    </AppProviders>,
     root
   )
 }
