b50c846bf1eaa0420e5f03541c8e6e3d3ff24d96
cballevre <celestin.ballevre@protonmail.com>
2022-11-21 20:14:57 +0100

    refactor(cleanTrashedGroups): extract queries to specific file

--- src/thunks/cleanTrashedGroups.js
+++ src/thunks/cleanTrashedGroups.js
@@ -1,40 +1,34 @@
-import { Q } from 'cozy-client'
-import { DOCTYPE_CONTACT_GROUPS, DOCTYPE_CONTACTS } from '../helpers/doctypes'
+import {
+  buildContactGroupsTrashedQuery,
+  buildContactsQueryByGroupId
+} from '../queries/queries'
+import { DOCTYPE_CONTACTS } from '../helpers/doctypes'
 
-const cleanTrashedGroups = () => async (dispatch, getState, { client }) => {
-  const { data: trashedGroups } = await client.query(
-    Q(DOCTYPE_CONTACT_GROUPS)
-      .where({ trashed: true })
-      .indexFields(['trashed'])
-  )
-  //eslint-disable-next-line
-  for (const trashedGroup of trashedGroups) {
-    await removeGroupFromAllContacts(client, trashedGroup._id)
-    await client.destroy(trashedGroup)
+const cleanTrashedGroups = () => {
+  return async (dispatch, getState, { client }) => {
+    const contactGroupsTrashedQuery = buildContactGroupsTrashedQuery()
+
+    const { data: trashedGroups } = await client.query(
+      contactGroupsTrashedQuery.definition
+    )
+    //eslint-disable-next-line
+    for (const trashedGroup of trashedGroups) {
+      await removeGroupFromAllContacts(client, trashedGroup._id)
+      await client.destroy(trashedGroup)
+    }
+
+    dispatch({
+      type: 'CLEAN_TRASHED_GROUPS',
+      groups: trashedGroups
+    })
   }
-
-  dispatch({
-    type: 'CLEAN_TRASHED_GROUPS',
-    groups: trashedGroups
-  })
 }
 
 const removeGroupFromAllContacts = async (client, groupId) => {
+  const contactQueryByGroupId = buildContactsQueryByGroupId(groupId)
+
   const { data, next: hasMore } = await client.query(
-    Q(DOCTYPE_CONTACTS)
-      .where({
-        relationships: {
-          groups: {
-            data: {
-              $elemMatch: {
-                _id: groupId,
-                _type: DOCTYPE_CONTACT_GROUPS
-              }
-            }
-          }
-        }
-      })
-      .indexFields(['relationships.groups.data'])
+    contactQueryByGroupId.definition
   )
 
   const contacts = client.hydrateDocuments(DOCTYPE_CONTACTS, data)
