1df18f2f0f33692b83d95c7949ab055e8655bac1
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-07-24 14:25:34 +0200

    refactor: Use component instead of function for rendering contact form fields

--- src/components/ContactCard/ContactForm/ContactFormField.jsx
+++ src/components/ContactCard/ContactForm/ContactFormField.jsx
@@ -5,48 +5,16 @@ import { FieldArray } from 'react-final-form-arrays'
 
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import Icon from 'cozy-ui/transpiled/react/Icon'
-import IconButton from 'cozy-ui/transpiled/react/IconButton'
-import CrossCircleIcon from 'cozy-ui/transpiled/react/Icons/CrossCircle'
 import PlusIcon from 'cozy-ui/transpiled/react/Icons/Plus'
-import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
 import { Media, Img, Bd } from 'cozy-ui/transpiled/react/deprecated/Media'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
-import { addField, removeField } from '../../../helpers/fields'
+import ContactFormFieldArrayItem from './ContactFormFieldArrayItem'
+import { addField } from '../../../helpers/fields'
 
 const ContactFormField = ({ name, icon, isArray, renderInput }) => {
   const { t } = useI18n()
-
   const fieldID = uniqueId('field_')
-
-  const renderArrayField = (
-    fields,
-    index,
-    nameWithIndex,
-    name,
-    renderInput
-  ) => {
-    const showRemove = fields.value[index]?.[name]
-
-    return (
-      <div key={nameWithIndex} className="u-mt-1 u-flex u-pos-relative">
-        {renderInput(`${nameWithIndex}.${name}`, fieldID)}
-
-        {showRemove && (
-          <ListItemIcon className="contact-form-field__delete-icon">
-            <IconButton
-              aria-label="delete"
-              color="error"
-              size="medium"
-              onClick={() => removeField(fields, index)}
-            >
-              <Icon icon={CrossCircleIcon} />
-            </IconButton>
-          </ListItemIcon>
-        )}
-      </div>
-    )
-  }
 
   return (
     <Media align="top" className="contact-form-field">
@@ -66,15 +34,17 @@ const ContactFormField = ({ name, icon, isArray, renderInput }) => {
           <FieldArray name={name}>
             {({ fields }) => (
               <div className="u-mt-1 u-mb-half">
-                {fields.map((nameWithIndex, index) =>
-                  renderArrayField(
-                    fields,
-                    index,
-                    nameWithIndex,
-                    name,
-                    renderInput
-                  )
-                )}
+                {fields.map((nameWithIndex, index) => (
+                  <ContactFormFieldArrayItem
+                    key={nameWithIndex}
+                    fields={fields}
+                    fieldID={fieldID}
+                    index={index}
+                    nameWithIndex={nameWithIndex}
+                    name={name}
+                    renderInput={renderInput}
+                  />
+                ))}
                 <Button
                   variant="text"
                   startIcon={<Icon icon={PlusIcon} />}
---
+++ src/components/ContactCard/ContactForm/ContactFormFieldArrayItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import IconButton from 'cozy-ui/transpiled/react/IconButton'
+import CrossCircleIcon from 'cozy-ui/transpiled/react/Icons/CrossCircle'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+
+import { removeField } from '../../../helpers/fields'
+
+const ContactFormFieldArrayItem = ({
+  fields,
+  fieldID,
+  index,
+  nameWithIndex,
+  name,
+  renderInput
+}) => {
+  const showRemove = fields.value[index]?.[name]
+
+  return (
+    <div className="u-mt-1 u-flex u-pos-relative">
+      {renderInput(`${nameWithIndex}.${name}`, fieldID)}
+
+      {showRemove && (
+        <ListItemIcon className="contact-form-field__delete-icon">
+          <IconButton
+            aria-label="delete"
+            color="error"
+            size="medium"
+            onClick={() => removeField(fields, index)}
+          >
+            <Icon icon={CrossCircleIcon} />
+          </IconButton>
+        </ListItemIcon>
+      )}
+    </div>
+  )
+}
+
+export default ContactFormFieldArrayItem
