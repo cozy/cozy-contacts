98bfc5aa5e1c01c23761109a8c4a23595cc8e935
Théo Poizat <hello@zatteo.com>
2025-06-06 14:03:23 +0200

    refactor: Centralize mutualized prop type in a file
    Avoid can't access lexical declaration 'X' before initialization

--- src/components/Form/FieldInputTextarea.jsx
+++ src/components/Form/FieldInputTextarea.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import TextField from 'cozy-ui/transpiled/react/TextField'
 
-import { FieldInputWrapperPropTypes } from './FieldInputWrapper'
+import { FieldInputWrapperPropTypes } from './fieldInputType'
 
 const FieldInputTextarea = props => {
   const { isMultiline, ...rest } = props
