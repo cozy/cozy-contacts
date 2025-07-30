9c4c0251413e4afcb72a7cd8c24906f857c70fc2
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-07-30 15:39:27 +0200

    feat(VirtualizedTable): Group contact by first letter

--- src/helpers/contactList.js
+++ src/helpers/contactList.js
@@ -25,3 +25,26 @@ export const categorizeContacts = (contacts, t) => {
     return acc
   }, {})
 }
+
+/**
+ * Counts how many contacts are categorized by first letter and store it in `groupCounts`
+ * Expl.: if there is 3 contacts in A and 2 in B, it will return [3,2]
+ * Also store first letters and store them in `groupLabels`
+ * @param {array} contacts - Array of io.cozy.contact documents
+ * @returns {object}
+ */
+export const makeGroupLabelsAndCounts = (contacts, t) => {
+  return contacts.reduce(
+    (acc, contact) => {
+      const header = makeHeader(contact, t)
+      if (!acc.groupLabels.includes(header)) {
+        acc.groupLabels.push(header)
+      }
+      const idx = acc.groupLabels.indexOf(header)
+      const val = acc.groupCounts[idx] || 0
+      acc.groupCounts[idx] = val + 1
+      return acc
+    },
+    { groupLabels: [], groupCounts: [] }
+  )
+}
